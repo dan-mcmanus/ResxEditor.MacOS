85f5c47aefaa989f67c4eab895e9cf53ed2c0749
Daniel McManus <dan@danmcmanus.dev>
2020-07-18 22:40:14 -0500

    Initial Commit

---
+++ Rexedit/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Rexedit
+//
+//  Created by Daniel McManus on 7/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rexedit/RexeditApp.swift
@@ -0,0 +1,17 @@
+//
+//  RexeditApp.swift
+//  Rexedit
+//
+//  Created by Daniel McManus on 7/18/20.
+//
+
+import SwiftUI
+
+@main
+struct RexeditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
